2af4eeb2c112fb2590895df342f299416ffe10b8
+ <+dannaewaltz@gmail.com>
2022-06-09 18:06:56 -0600

    commit tree e7d131470bca357a6bfa7a2d78ae92fdb97a7259

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove("main");
+
+const newHeader = document.createElement("h1")
+newHeader;
+
+// document.body.append(newHeader);
+
+// newHeader.nodeName = ("h1#victory"); 
+
+newHeader.id = "victory";
+newHeader.innerHTML = "Danna is the champion!";
+
+
+
+
+
+
+
+
+
